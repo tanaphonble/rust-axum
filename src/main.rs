f65861ad5525e8ecfefc46a67667df9b8eb4393b
aing <punyaporn.t@arise.tech>
2025-02-19 02:43:36 +0700

    refactor: handler, test handler

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-use axum::response::Html;
+use axum::response::{Html, IntoResponse};
 use axum::routing::get;
 use axum::Router;
 
 #[tokio::main]
 async fn main() {
-    let route_hello: Router<> = Router::new().route(
+    let route_hello = Router::new().route(
         "/hello",
-        get(|| async { Html("Hello <strong>World!!!</strong>") }),
+        get(handler_hello),
     );
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, route_hello).await.unwrap();
+}
+
+
+async fn handler_hello() -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello", "HANDLER");
+    Html("Hello <strong>World!!!</strong>")
 }
